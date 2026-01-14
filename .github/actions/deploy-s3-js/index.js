1035f98591a2fba7b0db08923fd5356286868fb8
nvbhoang <nvbhoang@cmcglobal.vn>
2026-01-14 16:06:43 +0700

    Push dist file to S3

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -4,6 +4,13 @@ const exec = require("@actions/exec");
 
 // NOTE: name can be anything
 function run() {
+  const bucket = core.getInput("bucket", { required: true });
+  const region = core.getInput("region", { required: true });
+  const distFolderPath = core.getInput("dist-folder", { required: true });
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolderPath} ${s3Uri} --region ${region}`);
+
   console.log("Hello world");
   core.notice("Hello world but from core");
 }
