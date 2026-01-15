5498a671ee1c1f1bda718eaf676050016742a70f
nvbhoang <nvbhoang@cmcglobal.vn>
2026-01-15 10:20:55 +0700

    Add s3 website url to output

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -3,16 +3,25 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 // NOTE: name can be anything
-function run() {
-  const bucket = core.getInput("bucket", { required: true });
-  const region = core.getInput("region", { required: true });
-  const distFolderPath = core.getInput("dist-folder", { required: true });
+async function run() {
+  try {
+    const bucket = core.getInput("bucket", { required: true });
+    const region = core.getInput("region", { required: true });
+    const distFolderPath = core.getInput("dist-folder", { required: true });
 
-  const s3Uri = `s3://${bucket}`;
-  exec.exec(`aws s3 sync ${distFolderPath} ${s3Uri} --region ${region}`);
+    const s3Uri = `s3://${bucket}`;
+    await exec.exec(
+      `aws s3 sync ${distFolderPath} ${s3Uri} --region ${region}`
+    );
 
-  console.log("Hello world");
-  core.notice("Hello world but from core");
+    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    core.setOutput("website-url", websiteUrl);
+
+    console.log("Deployment successful!");
+    core.notice(`Website deployed to: ${websiteUrl}`);
+  } catch (error) {
+    core.setFailed(`Deployment failed: ${error.message}`);
+  }
 }
 
 run();
